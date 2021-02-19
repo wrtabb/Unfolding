5c3338a56a68b8bdc0b7b90c3d86fbfd9fe487e6
Robert Tabb <wrtabb@gmail.com>
2021-02-19 10:39:11 -0600

    added toy model functionality

---
+++ include/ToyModel.hh
@@ -0,0 +1,59 @@
+#ifndef ToyModel_HH
+#define ToyModel_HH
+#include <TMath.h>
+#include <TF1.h>
+#include <TF2.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <Math/PdfFuncMathCore.h>
+#include "FunctionalModels.h"
+
+class ToyModel
+{
+	public:
+		ToyModel(double distNorm,double expDecay,double peakNormRel,
+			 double distMean,double distSigma,double resSigma,
+			 double xmin,double xmax,int nBins);
+		TH1F*GetTrueHist();
+		TH1F*GetRecoHist(bool useTUnfold = false);
+		TH2F*GetMigrationMatrix(bool useTUnfold = false);
+	private:
+		double _distNorm;
+		double _expDecay;
+		double _peakNormRel;
+		double _distMean;
+		double _distSigma;
+		double _resSigma;
+		double _xmin;
+		double _xmax;
+		int _nBins;
+		
+		TF1*_trueFunc;
+		TF1*_recoFunc;
+		TF2*_matrixFunc;
+		void SetModelParameters(double distNorm,double expDecay,double peakNormRel,
+					double distMean,double distSigma,double resSigma,
+					double xmin,double xmax,int nBins);
+		void SetModelFunctions(TF1*trueFunc,TF1*recoFunc,TF2*matrixFunc);
+		TF1*Get1DIntegrand();
+		TF2*Get2DIntegrand();
+};
+
+double trueDistribution(double*var,double*par);
+double resolutionFunction(double*var,double*par);
+double recoDistribution(double*var,double*par);
+double integrand1D(double*var,double*par);
+double integrand2D(double*var,double*par);
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/ToyModel.cc
@@ -0,0 +1,125 @@
+#include "../include/ToyModel.hh"
+
+ToyModel::ToyModel(double distNorm,double expDecay,double peakNormRel,double distMean,
+		   double distSigma,double resSigma,double xmin,double xmax,int nBins)
+{
+	SetModelParameters(distNorm,expDecay,peakNormRel,distMean,distSigma,resSigma,xmin,xmax,
+			   nBins);
+
+	TF1*trueFunc = new TF1("trueFunction",trueDistribution,0,100,5);	
+	trueFunc->SetParameters(distNorm,expDecay,peakNormRel,distMean,distSigma);
+	
+	TF1*recoFunc = new TF1("recoFunction",recoDistribution,-10,150,6);
+	recoFunc->SetParameters(distNorm,expDecay,peakNormRel,distMean,distSigma,resSigma);
+
+	TF2*matrixFunc = new TF2("integrand2DFunc", integrand2D,0,100,-10,150,6);
+	matrixFunc->SetParameters(distNorm,expDecay,peakNormRel,distMean,distSigma,resSigma);
+
+	SetModelFunctions(trueFunc,recoFunc,matrixFunc);
+}
+
+void ToyModel::SetModelFunctions(TF1*trueFunc,TF1*recoFunc,TF2*matrixFunc)
+{
+	_trueFunc = trueFunc;
+	_recoFunc = recoFunc;
+	_matrixFunc = matrixFunc;
+}
+
+void ToyModel::SetModelParameters(double distNorm,double expDecay,double peakNormRel,
+				  double distMean,double distSigma,double resSigma,double xmin, 				 double xmax,int nBins)
+{
+	_distNorm = distNorm;
+	_expDecay = expDecay;
+	_peakNormRel = peakNormRel;
+	_distMean = distMean;
+	_distSigma = distSigma;
+	_resSigma = resSigma;
+	_xmin = xmin;
+	_xmax = xmax;
+	_nBins = nBins;
+}
+
+TH1F*ToyModel::GetTrueHist()
+{
+	TF1*func = _trueFunc;
+	func->SetParameters(_distNorm,_expDecay,_peakNormRel,_distMean,_distSigma,_resSigma);	
+	TH1F*hist = new TH1F("hTrue","",_nBins,0,100);
+	hist->SetFillColor(kRed+2);
+	hist->SetLineColor(kRed+2);
+
+	double content;
+	double binMin;
+	double binMax;
+	for(int i=1;i<=_nBins;i++){
+		binMin = hist->GetXaxis()->GetBinLowEdge(i);
+		binMax = hist->GetXaxis()->GetBinUpEdge(i);
+		content = func->Integral(binMin,binMax);
+		hist->SetBinContent(i,content);
+		hist->SetBinError(i,1/sqrt(content));
+	}
+
+	return hist;
+}
+
+
+TH1F*ToyModel::GetRecoHist(bool useTUnfold)
+{
+	int nBins;
+	if(useTUnfold) nBins = _nBins*2; //TUnfold requires more reco bins than true
+	else nBins = _nBins;
+	TF1*func = new TF1("func",recoDistribution,_xmin,_xmax,6);
+	func->SetParameters(_distNorm,_expDecay,_peakNormRel,_distMean,_distSigma,_resSigma);	
+	TH1F*hist = new TH1F("hReco","",_nBins,_xmin,_xmax);
+	hist->SetLineColor(kBlack);
+	hist->SetMarkerStyle(20);
+	hist->SetMarkerColor(kBlack);
+
+	double content;
+	double binMin;
+	double binMax;
+	for(int i=1;i<=_nBins;i++){
+		binMin = hist->GetXaxis()->GetBinLowEdge(i);
+		binMax = hist->GetXaxis()->GetBinUpEdge(i);
+		content = func->Integral(binMin,binMax);
+		hist->SetBinContent(i,content);
+		hist->SetBinError(i,1/sqrt(content));
+	}
+	return hist;
+}
+
+TH2F*ToyModel::GetMigrationMatrix(bool useTUnfold)
+{
+	int nBinsTrue = _nBins;
+	int nBinsReco;
+	double xMin = _xmin-7*_resSigma;
+	double xMax = _xmax+7*_resSigma;
+	if(useTUnfold) nBinsReco = 2*nBinsTrue;
+	else nBinsReco = nBinsTrue;
+	TF2*integrand2DFunc = new TF2("integrand2DFunc",integrand2D,xMin,xMax,xMin,xMax,6);
+	integrand2DFunc->SetParameters(_distNorm,_expDecay,_peakNormRel,_distMean,_distSigma,
+				       _resSigma);
+
+	TH2F*migrationHist = new TH2F("migrationHist", "migrationHist",nBinsReco,_xmin,_xmax,
+			              nBinsTrue,_xmin,_xmax);
+	double xlow,xhi,ylow,yhi,yield;
+        for(int iBin = 0; iBin <= nBinsReco+1; iBin++){//loop over columns
+                for(int jBin = 0; jBin <= nBinsTrue+1; jBin++){//looop over rows
+                        if(iBin==0) xlow = xMin;
+                        else xlow = migrationHist->GetXaxis()->GetBinLowEdge(iBin);
+
+                        if(iBin==nBinsReco+1) xhi = xMax;
+                        else xhi  = migrationHist->GetXaxis()->GetBinUpEdge (iBin);
+
+                        if(jBin==0) ylow = xMin;
+                        else ylow = migrationHist->GetYaxis()->GetBinLowEdge(jBin);
+
+                        if(jBin==nBinsTrue+1) yhi = xMax;
+                        else  yhi  = migrationHist->GetYaxis()->GetBinUpEdge (jBin);
+
+                        double content = integrand2DFunc->Integral(xlow, xhi, ylow, yhi);
+                        migrationHist->SetBinContent(iBin,jBin,content);
+			if(content!=0) migrationHist->SetBinError(iBin,jBin,1/sqrt(content));
+                }//end loop over rows
+        }//end loop over columns
+	return migrationHist;
+}
